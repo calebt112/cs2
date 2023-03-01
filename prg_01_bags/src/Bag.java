a93fcf2f54c923796913024c9cdece5e1bf3bd17
Caleb <cthom112@msudenver.edu>
2023-03-01 16:41:34 -0700

    Completed:
    Todo #1, #2, #3, #4, #5, #7
    
    Needs work:
    
    Todo #6, #14, #15
    
    Untested:
    
    Todo #8, #9, #10, #11, #12,
    #13,

--- prg_01_bags/src/Bag.java
+++ prg_01_bags/src/Bag.java
@@ -14,69 +14,141 @@ public class Bag {
 
     public static int DEFAULT_WEIGHT_LIMIT = 50;
 
-    // TODO #3: set weightLimit to DEFAULT_WEIGHT_LIMIT if provided value is less than or equal to zero;
+    // TODOd #3: set weightLimit to DEFAULT_WEIGHT_LIMIT if provided value is less than or equal to zero;
     // set head to null; and weight to zero
     public Bag(double weightLimit) {
-
+        if(weightLimit <= 0)
+            this.weightLimit = DEFAULT_WEIGHT_LIMIT;
+        head = null;
+        weight = 0;
     }
 
-    // TODO #4: same as constructor above, setting weightLimit to DEFAULT_WEIGHT_LIMIT
+    // TODOd #4: same as constructor above, setting weightLimit to DEFAULT_WEIGHT_LIMIT
     public Bag() {
-
+        weightLimit = DEFAULT_WEIGHT_LIMIT;
+        head = null;
+        weight = 0;
     }
 
-    // TODO #5: return true/false depending on whether the bas is empty of not
+    // TODOd #5: return true/false depending on whether the bag is empty of not
     public boolean isEmpty() {
+        Node<Item> current = head;
+        if(current == null)
+            return true;
         return false;
     }
 
-    // TODO #6: return the (current) weight of the bag
+    // TODOd #6: return the (current) weight of the bag
     public double weight() {
-        return 0;
+        Node<Item> current = head;
+        while(current != null){
+            weight += current.getValue().getWeight();
+            current = current.getNext();
+        }
+        return weight;
     }
 
-    // TODO #7: return the weight limit of the bag
+    // TODOd #7: return the weight limit of the bag
     public double weightLimit() {
-        return 0;
+        return weightLimit;
     }
 
     // TODO #8: add the given item to the bag only if it doesn't make the bag heavier than its weight limit;
     // item added always becomes the new head node; return true if operation is successful; false otherwise
     public boolean add(final Item item) {
-        return false;
+        Node<Item> newNode = new Node<Item>(item);
+        if(weight() + item.getWeight() > weightLimit)
+            return false;
+        else{
+            newNode.setNext(head);
+            head = newNode;
+            return true;
+        }
     }
 
     // TODO #9: return the total number of items in the bag
     public int count() {
-        return 0;
+        Node<Item> current = head;
+        int count = 0;
+        while(current != null){
+            count++;
+            current = current.getNext();
+        }
+        return count;
     }
 
     // TODO #10: return the number of items that are equal to the given item
     public int count(final Item item) {
-        return 0;
+        Node<Item> current = head;
+        int count = 0;
+        while(current != null){
+            if(current.getValue().equals(item))
+                count++;
+            current = current.getNext();
+        }
+        return count;
     }
 
     // TODO #11: return the total price of the bag (i.e., the sum of the prices of all items in the bag);
     // if the bag is empty, return zero
     public int price() {
-        return 0;
+        Node<Item> current = head;
+        int price = 0;
+        while(current != null){
+            price += current.getValue().getPrice();
+            current = current.getNext();
+        }
+        return price;
     }
 
     // TODO #12: return a reference to the most pricey item in the bag;
     // if the bag has multiple most pricey items, return the first one found;
     // if the bag is empty, return null
     public Item mostPricey() {
-       return null;
+        Node<Item> current = head;
+        Node<Item> mostPricey = head;
+       if(current == null)
+           return null;
+       else {
+           while (current != null) {
+               if (current.getValue().compareTo(mostPricey.getValue()) < 0) {
+                   mostPricey = current;
+                   current = current.getNext();
+               } else
+                   current = current.getNext();
+           }
+           return mostPricey.getValue();
+       }
     }
+
 
     // TODO #13: return true/false depending on whether the item is in the bag or not
     public boolean contains(final Item item) {
+        Node<Item> current = head;
+        while(current != null){
+            if(current.getValue().equals(item))
+                return true;
+            current = current.getNext();
+        }
         return false;
     }
 
     // TODO #14: remove the first occurrence of the item from the bag;
     // return true if operation is successful; false otherwise
     public boolean remove(final Item item) {
+        Node<Item> current = head;
+        Node<Item> previous = null;
+        while(current != null){
+            if(current.getValue().equals(item)){
+                if(previous == null)
+                    head = current.getNext();
+                else
+                    previous.setNext(current.getNext());
+                current = current.getNext();
+                return true;
+            }
+            current = current.getNext();
+        }
         return false;
     }
 
