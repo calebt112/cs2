7269f7494c3487f7fce73a23a79a586767148dbf
Caleb <cthom112@msudenver.edu>
2023-03-06 10:53:59 -0700

    Homework 4 completed

---
+++ hwk_04_linked_list/src/LinkedListDriver.java
@@ -0,0 +1,27 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Description: Activity 06 - LinkedListDriver
+ */
+
+import java.util.Iterator;
+
+public class LinkedListDriver {
+    public static void main(String[] args) {
+
+        // TODOd #2: create a linked list with the pets used in README.  Display the linked list, reverse it, and then display it again
+
+        LinkedList<String> pets = new LinkedList<>();
+        pets.add("Spike");
+        pets.add("Rex");
+        pets.add("Spots");
+        pets.add("Sparky");
+
+        System.out.println(pets);
+
+        pets.reverse();
+
+        System.out.println(pets);
+        
+    }
+}
