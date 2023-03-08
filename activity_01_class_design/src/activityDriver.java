9a654954fb6343bb0743d97f14b22d153fae854d
Caleb Thompson <cthom112@msudenver.edu>
2023-03-08 10:14:15 -0700

    Activity 01 - class design. Driver file - sandwich class method demo

--- activity_01_class_design/src/activityDriver.java
+++ activity_01_class_design/src/activityDriver.java
@@ -1,6 +1,7 @@
 public class activityDriver {
     public static void main(String[] args) {
 
+        // Account class method calls
         // create a new account object
         Account account = new Account(1122, "Caleb");
         // deposit $20,000
@@ -9,5 +10,13 @@ public class activityDriver {
         account.withdraw(2500);
         // display account
         System.out.println(account);
+
+        // Sandwich class method calls
+        // create a new sandwich object
+        Sandwich sandwich = new Sandwich("Turkey", 245, 4.99);
+        // check isHealthy
+        System.out.println(sandwich.isHealthy());
+        // display sandwich
+        System.out.println(sandwich);
     }
 }
