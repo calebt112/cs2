3c2d0b453a2141b2e743c2a06309a703af0a068b
Caleb <cthom112@msudenver.edu>
2023-03-07 22:00:47 -0700

    Lab 4 completed

---
+++ lab_04_circular_list/src/CircularLinkedList.java
@@ -0,0 +1,88 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Student Name: Thompson Caleb
+ * Description: Lab 04 - CircularLinkedList
+ */
+
+import java.util.Iterator;
+
+public class CircularLinkedList<E> implements Iterable<E> {
+
+    private Node<E> head;
+
+    public CircularLinkedList() {
+        head = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    /**
+     * TODO #4: add a new element (with the value) at the end of the list; because the list is circular, the tail node is the one that references the head
+     * @param value
+     */
+    public void append(E value) {
+        Node<E> newNode = new Node<>(value);
+        if(isEmpty()) {
+            head = newNode;
+            head.setNext(head);
+        }
+        if(head.getNext() == head) {
+            head.setNext(newNode);
+            newNode.setNext(head);
+        }
+        else {
+            Node<E> current = head;
+            while(current.getNext() != head) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+            newNode.setNext(head);
+        }
+    }
+
+    /**
+     * TODO #5: returns the number of elements in the list
+     */
+    public int size() {
+        int count = 0;
+        if(isEmpty())
+            return count;
+        else {
+            Node<E> current = head;
+            while(current.getNext() != head) {
+                count++;
+                current = current.getNext();
+            }
+            count++;
+        }
+        return count;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                // TODO #6: if the list is not empty, next should always return a value
+                if(isEmpty())
+                    return null;
+                else {
+                    E value = current.getValue();
+                    current = current.getNext();
+                    return value;
+                }
+            }
+        };
+    }
+}
---
+++ lab_04_circular_list/src/Task.java
@@ -0,0 +1,71 @@
+/*
+ * CS2050 - Computer Science II - Spring 2023
+ * Instructor: Thyago Mota
+ * Student Name: Thompson Caleb
+ * Description: Lab 04 - Task
+ */
+
+public class Task {
+
+    private int size;
+    private int priority;
+    private boolean dead;
+
+    public static final int LOW_PRIORITY = 1;
+    public static final int STANDARD_PRIORITY = 2;
+    public static final int HIGH_PRIORITY  = 3;
+    private static final int MIN_TASK_SIZE = 1;
+
+    /**
+     * TODO #1: a task has size > 0 (set it to 1 if given value is invalid) and a priority (1, 2, or 3); tasks are always created in the "alive" state
+     * @param size it has to be > 0
+     * @param priority it has to be 1, 2, or 3
+     */
+    public Task(int size, int priority) {
+        if(size < MIN_TASK_SIZE)
+            this.size = MIN_TASK_SIZE;
+        else
+            this.size = size;
+        if(priority == LOW_PRIORITY || priority == STANDARD_PRIORITY || priority == HIGH_PRIORITY)
+            this.priority = priority;
+        else
+            this.priority = STANDARD_PRIORITY;
+        dead = false;
+    }
+
+    /**
+     * TODO #2: this creates a task with a (given) size and standard priority
+     * @param size
+     */
+    public Task(int size) {
+        if(size < MIN_TASK_SIZE)
+            this.size = MIN_TASK_SIZE;
+        else
+            this.size = size;
+        this.priority = STANDARD_PRIORITY;
+        dead = false;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    /**
+     * TODO #3: if the task is alive, run the task one unit of time by decrementing its size; update the task status if its size becomes zero
+     */
+    public void run() {
+        if(!dead) {
+            size--;
+            if(size == 0)
+                dead = true;
+        }
+    }
+}
