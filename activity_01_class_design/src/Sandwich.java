40b96eb66fdbb5b5eafcc8fd7ff0706bef7dac05
Caleb Thompson <cthom112@msudenver.edu>
2023-03-08 09:56:43 -0700

    Activity 01 - class design. Sandwich constructor completed

--- activity_01_class_design/src/Sandwich.java
+++ activity_01_class_design/src/Sandwich.java
@@ -5,4 +5,22 @@
  */
 
 public class Sandwich {
+
+    private String name;
+    private int calories;
+    private double price;
+    final int MIN_CALORIES = 0;
+    final int MIN_PRICE = 0;
+
+    public Sandwich(String name, int calories, double price) {
+        this.name = name;
+        if(calories <= MIN_CALORIES)
+            this.calories = MIN_CALORIES;
+        else
+            this.calories = calories;
+        if(price <= MIN_PRICE)
+            this.price = MIN_PRICE;
+        else
+            this.price = price;
+    }
 }
