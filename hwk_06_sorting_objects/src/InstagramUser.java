cd0fb3e82559a6777175d9ef9b07d166352a2f2c
Caleb Thompson <cthom112@msudenver.edu>
2023-04-07 08:14:11 -0600

    Homework 6 - completed

--- hwk_06_sorting_objects/src/InstagramUser.java
+++ hwk_06_sorting_objects/src/InstagramUser.java
@@ -1,15 +1,14 @@
-]/*
+/*
  * CS2050 - Computer Science II - Fall 2022
  * Instructor: Thyago Mota
- */
  * Description: Homework 06 - Sorting Objects
- * Student name: Caleb Thompson
+ * Student name:
  */
-
+import java.util.Arrays;
 public class InstagramUser implements Comparable<InstagramUser> {
 
     private String username;
-    private int    followers;
+    private int followers;
 
     public InstagramUser(String username, int followers) throws InstantiationException {
         this.username = username;
@@ -17,13 +16,13 @@ public class InstagramUser implements Comparable<InstagramUser> {
         this.followers = followers;
     }
 
-    //TODO #1: override compareTo such that more popular users (higher number of followers) appear before less popular users
+    // TODOd #1: override compareTo such that more popular users (higher number of followers) appear before less popular users
     @Override
     public int compareTo(InstagramUser other) {
-        if(this.followers > other.followers)
-            return 1;
-        else if(this.followers < other.followers)
+        if (this.followers > other.followers)
             return -1;
+        else if (this.followers < other.followers)
+            return 1;
         else
             return 0;
     }
@@ -36,30 +35,31 @@ public class InstagramUser implements Comparable<InstagramUser> {
                 '}';
     }
 
-    // TODO #2: adapt partition so it would work with any Comparable array hint: change the type of data from int to Comparable
-    private static int partition(int data[], int start, int end) {
+    // TODOd #2: adapt partition so it would work with any Comparable array
+    // hint: change the type of data from int to Comparable
+    private static int partition(Comparable data[], int start, int end) {
         int pivot = end;
         int partition = start;
         int current = start;
         while (current < pivot) {
             // hint: change the if expression
-            if (data[current] < data[pivot]) {
-                int temp = data[partition]; // hint: change the type of data from int to Comparable
+            if (data[current].compareTo(data[pivot]) < 0) {
+                Comparable temp = data[partition];
                 data[partition] = data[current];
                 data[current] = temp;
                 partition++;
             }
             current++;
         }
-        int temp = data[pivot]; // change the type of data from int to Comparable
+        Comparable temp = data[pivot];
         data[pivot] = data[partition];
         data[partition] = temp;
         return partition;
     }
 
-    // TODO #3: adapt the quick sort algorithm so it would work with any Comparable array
+    // TODOd #3: adapt the quick sort algorithm so it would work with any Comparable array
     // hint: change the type of data from int to Comparable
-    public static void quickSort(int data[], int start, int end) {
+    public static void quickSort(Comparable data[], int start, int end) {
         if (start < end) {
             int partition = partition(data, start, end);
             quickSort(data, start, partition - 1);
@@ -68,11 +68,20 @@ public class InstagramUser implements Comparable<InstagramUser> {
     }
 
     public static void main(String[] args) throws InstantiationException {
-        // TODO #4: create a static array of 5 instagram accounts
-        Comparable instaUsers[] = {
+        // TODOd #4: create a static array of 5 Instagram accounts
+        InstagramUser[] instaUsers = {
+                new InstagramUser("user1", 500),
+                new InstagramUser("user2", 10000),
+                new InstagramUser("user3", 22000),
+                new InstagramUser("user4", 80),
+                new InstagramUser("user5", 6000)
         };
 
-        // TODO #5: sort the accounts from most popular to least popular using quick sort and then display the accounts
-
+        // TODOd #5: sort the accounts from most popular to least popular using quick sort and then display the accounts
+        quickSort(instaUsers, 0, instaUsers.length - 1);
+        System.out.println(Arrays.toString(instaUsers));
     }
 }
+
+
+
